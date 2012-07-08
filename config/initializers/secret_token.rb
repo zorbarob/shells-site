d9913e486c77a6a1b537036f8fb21ac0f0935086
Robert Byars <zorbarob@gmail.com>
2012-07-08 01:11:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShellsSite::Application.config.secret_token = 'b27169b2a8f8f7ed03dae142fc25ef8328c564c45fadf7015111faa8e3e821445d2462c1a6c1d933c7e3ed00f0d837402bbd277c4ba2f769c09c5f74bf4c05d9'
